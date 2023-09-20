d097dbcdf1696989395be129be0fefa4e0167a5f
Randy <monyerandy15@gmail.com>
2023-09-20 20:21:00 +0100

    First Push

---
+++ components/base/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="max-w-screen-xl px-8 mx-auto">
+    <div class="py-4 sm:py-4 text-center text-sm text-[#555555]">
+      &copy; Copyright {{ currentYear }}
+      <span class="underline hover:text-black"
+        ><a href="mailto:monyerandy@yahoo.com">Randy Monye</a></span
+      >. All rights reserved
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "BaseFooter",
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    };
+  },
+};
+</script>
---
+++ components/base/Navbar.vue
@@ -0,0 +1,70 @@
+<template>
+  <nav id="navbar" class="relative z-10 w-full text-neutral-800">
+    <div
+      class="flex flex-col max-w-screen-xl px-8 mx-auto lg:items-center lg:justify-between lg:flex-row py-4"
+    >
+      <div class="flex flex-col lg:flex-row items-center space-x-4 xl:space-x-8">
+        <div class="w-full flex flex-row items-center justify-between py-6">
+          <div>
+            <nuxt-link to="/" class="text-[#0c66ee] font-bold">MailToSelf</nuxt-link>
+          </div>
+          <button
+            class="rounded-lg lg:hidden focus:outline-none focus:shadow-outline"
+            @click="open = !open"
+          >
+            <SegmentIcon v-if="!open" :size="24" />
+            <CloseIcon v-else :size="24" />
+          </button>
+        </div>
+        <ul
+          :class="[open ? 'flex' : 'hidden lg:flex']"
+          class="text-gray-500 w-full h-auto flex flex-col flex-grow lg:items-center pb-4 lg:pb-0 lg:justify-center lg:flex-row origin-top duration-300 xl:space-x-2 space-y-3 lg:space-y-0"
+        >
+          <nuxt-link to="/public-letters" class="cursor-pointer truncate-text mr-4"
+            >Write a Letter</nuxt-link
+          >
+          <nuxt-link to="/public-letters" class="cursor-pointer truncate-text"
+            >Public Letters</nuxt-link
+          >
+        </ul>
+      </div>
+      <div :class="[open ? 'flex' : 'hidden lg:flex']" class="space-x-3">
+        <base-button
+          class="px-8 xl:px-10 py-3 mt-2 bg-inherit text-gradient border border-[#0c66ee]"
+        >
+          Login
+        </base-button>
+        <base-button
+          class="px-8 xl:px-10 py-3 mt-2 bg-gradient-to-r from-[#468ef9] to-[#0c66ee] text-white"
+        >
+          Sign Up
+        </base-button>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: "BaseNavbar",
+  data() {
+    return {
+      open: false,
+      dropdownNavbar: false,
+    };
+  },
+  methods: {
+    dropdownToggler() {
+      this.dropdownNavbar = !this.dropdownNavbar;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.truncate-text {
+  white-space: nowrap; /* Prevent wrapping */
+  overflow: hidden; /* Hide overflow */
+  text-overflow: ellipsis; /* Display ellipsis (...) for truncated text */
+  max-width: 150px; /* Adjust the max-width as needed */
+}
+</style>
